459a003d5ad4b9d902e0df4128d2c0a6f9d71c4d
bolson <bolson@ndwol01>
2012-05-01 13:06:29 -0400

    Fixed skip back and forward to stop and then resume playing a track if there is one currently playing.

--- VAMP/src/com/player/bll/VampPlayer.java
+++ VAMP/src/com/player/bll/VampPlayer.java
@@ -59,14 +59,24 @@ public class VampPlayer {
     }
 
     public void skipForward() {
+      boolean wasPlaying = isPlaying();
+      stop();
         if ((playlistIndex + 1) < playlist.size()) {
             ++playlistIndex;
+        }
+        if ( wasPlaying ) {
+          play();
         }
     }
 
     public void skipBackward() {
+      boolean wasPlaying = isPlaying();
+      stop();
         if ((playlistIndex - 1) >= 0) {
             --playlistIndex;
+        }
+        if ( wasPlaying ) {
+          play();
         }
     }
 
